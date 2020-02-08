482b1666e84ba8cb72e3728b5d526a799d688f6f
Benjamin Killer <benji_killer@hotmail.de>
2020-02-08 13:57:45 +0100

    add 404 routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -14,6 +14,10 @@ const routes: Routes = [
         path: 'gewicht',
         pathMatch: 'full',
         component: AppWeightComponent
+    },
+    {
+        path: '**',
+        component: AppShopComponent
     }
 ];
 
